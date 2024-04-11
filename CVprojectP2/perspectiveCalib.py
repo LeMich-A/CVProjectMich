de6582b16d47ccd691185b003f12b91b668d2797
LeMich-A <makpro2002@gmail.com>
2024-04-11 08:10:17 -0400

    new add

--- CVprojectP2/perspectiveCalib.py
+++ CVprojectP2/perspectiveCalib.py
@@ -3,52 +3,147 @@ import cv2 as cv
 
 #Load in initial camera calibration parameters
 savedir = 'calibration_data/'
-
+writeValues=False
 cam_mtx = np.load(savedir+'optimalIntrinsic.npy')
 rvecs = np.load(savedir+'rvecs.npy')
 tvecs = np.load(savedir+'tvecs.npy')
-#print(rvecs)
+dist = np.load(savedir+'dist.npy')
+#load center points from New Camera matrix
+cx=cam_mtx[0,2]
+cy=cam_mtx[1,2]
+fx=cam_mtx[0,0]
+print("cx: "+str(cx)+",cy "+str(cy)+",fx "+str(fx))
 
-# Initialize an identity matrix to accumulate rotations
-combined_rotation_matrix = np.eye(3)
-print(combined_rotation_matrix)
 
-# Initialize a zero vector to accumulate translations
-combined_translation_vector = np.zeros((1,3, 1))
-# Loop through each rotation vector
-for rvec, tvec in zip(rvecs, tvecs):
-    # Convert rotation vector to rotation matrix using Rodrigues formula
-    rotation_matrix, _ = cv.Rodrigues(rvec)
-    # Accumulate rotations by multiplying rotation matrices
-    combined_rotation_matrix = np.dot(combined_rotation_matrix, rotation_matrix)
-    combined_translation_vector += tvec
-print("combined rotation matrix from all sample frames")
-print(combined_rotation_matrix)
-print("combined translation vector")
-print(combined_translation_vector)
-# Construct transformation matrix using cv.composeRT()
-# Note: The composeRT function expects translation vector in shape (3, 1), not (1, 3, 1)
-combined_translation_vector = combined_translation_vector.reshape(3, 1)
-transform_matrix = np.hstack((combined_rotation_matrix, combined_translation_vector))
+#MANUALLY INPUT YOUR MEASURED POINTS HERE
+#ENTER (X,Y,d*)
+#d* is the distance from your point to the camera lens. (d* = Z for the camera center)
+#we will calculate Z in the next steps after extracting the new_cam matrix
 
-print("testing print of extrinsic parameters: ")
-print(transform_matrix)
 
-ProjectionMtx = cam_mtx.dot(transform_matrix)
-print(ProjectionMtx)
+#world center + 9 world points
 
-#to find scaling factor
-nickel_d = 21.25 #mm
-nick_radius = nickel_d/2
+total_points_used=9
 
-pixel_radius = 25.50 #pixels
+X_center=14
+Y_center=10
+Z_center=69
+loonie_rad = 26.5/2 #mm
+worldPoints=np.array([[X_center,Y_center,Z_center],                      
+                       [4,3,69],
+                       [14,3,67],
+                       [24,3,70],
+                       [4,9,70],
+                       [14,10,69],
+                       [23,10,70],
+                       [4,17,70],
+                       [14,17,70],
+                       [23,17,71]], dtype=np.float32)
 
-s = pixel_radius/nick_radius #pixels / mm
+#MANUALLY INPUT THE DETECTED IMAGE COORDINATES HERE
+pixel_rad = 30 #pixels
+#[u,v] center + 9 Image points
+imagePoints=np.array([[cx,cy],
+                       [180,92],
+                       [304,92],
+                       [436,90],
+                       [180,196],
+                       [310,198],
+                       [442,200],
+                       [178,290],
+                       [306,290],
+                       [444,288]], dtype=np.float32)
+                       
 
-#
-print(s)
-np.save(savedir+'scalingfactor.npy', s)
-np.save(savedir+'RT.npy', transform_matrix)
-np.save(savedir+'PMtx.npy', ProjectionMtx)
-np.save(savedir+'combinedRot.npy', combined_rotation_matrix)
-np.save(savedir+'combinedTranslation.npy', combined_translation_vector)
+#FOR REAL WORLD POINTS, CALCULATE Z from d*
+
+for i in range(0,total_points_used):
+    #start from 1, given for center Z=d*
+    #to center of camera
+    wX=worldPoints[i,0]-X_center
+    wY=worldPoints[i,1]-Y_center
+    wd=worldPoints[i,2]
+
+    d1=np.sqrt(np.square(wX)+np.square(wY))
+    wZ=np.sqrt(np.square(wd)-np.square(d1))
+    worldPoints[i,2]=wZ
+
+print(worldPoints)
+
+
+#print(ret)
+print("Camera Matrix")
+print(cam_mtx)
+
+
+print("New Camera Matrix")
+print(cam_mtx)
+inverse_newcam_mtx = np.linalg.inv(cam_mtx)
+print("Inverse New Camera Matrix")
+print(inverse_newcam_mtx)
+if writeValues==True: np.save(savedir+'inverse_newcam_mtx.npy', inverse_newcam_mtx)
+
+print(">==> Calibration Loaded")
+
+
+print("solvePNP")
+ret, rvec1, tvec1=cv.solvePnP(worldPoints,imagePoints,cam_mtx,dist)
+
+print("pnp rvec1 - Rotation")
+print(rvec1)
+if writeValues==True: np.save(savedir+'rvec1.npy', rvec1)
+
+print("pnp tvec1 - Translation")
+print(tvec1)
+if writeValues==True: np.save(savedir+'tvec1.npy', tvec1)
+
+print("R - rodrigues vecs")
+R_mtx, jac=cv.Rodrigues(rvec1)
+print(R_mtx)
+if writeValues==True: np.save(savedir+'R_mtx.npy', R_mtx)
+
+print("R|t - Extrinsic Matrix")
+Rt=np.column_stack((R_mtx,tvec1))
+print(Rt)
+if writeValues==True: np.save(savedir+'Rt.npy', Rt)
+
+print("newCamMtx*R|t - Projection Matrix")
+P_mtx=cam_mtx.dot(Rt)
+print(P_mtx)
+if writeValues==True: np.save(savedir+'P_mtx.npy', P_mtx)
+
+s_arr=np.array([0], dtype=np.float32)
+s_describe=np.array([0,0,0,0,0,0,0,0,0,0],dtype=np.float32)
+
+for i in range(0,total_points_used):
+    print("=======POINT # " + str(i) +" =========================")
+    
+    print("Forward: From World Points, Find Image Pixel")
+    XYZ1=np.array([[worldPoints[i,0],worldPoints[i,1],worldPoints[i,2],1]], dtype=np.float32)
+    XYZ1=XYZ1.T
+    print("{{-- XYZ1")
+    print(XYZ1)
+    suv1=P_mtx.dot(XYZ1)
+    print("//-- suv1")
+    print(suv1)
+    s=suv1[2,0]    
+    uv1=suv1/s
+    print(">==> uv1 - Image Points")
+    print(uv1)
+    print(">==> s - Scaling Factor")
+    print(s)
+    s_arr=np.array([s/total_points_used+s_arr[0]], dtype=np.float32)
+    s_describe[i]=s
+    if writeValues==True: np.save(savedir+'s_arr.npy', s_arr)
+
+    print("Solve: From Image Pixels, find World Points")
+
+    uv_1=np.array([[imagePoints[i,0],imagePoints[i,1],1]], dtype=np.float32)
+    uv_1=uv_1.T
+    print(">==> uv1")
+    print(uv_1)
+    suv_1=s*uv_1
+    print("//-- suv1")
+    print(suv_1)
+
+    np.save(savedir+'scalingfactor.npy', s)
--- CVprojectP2/videoprocessor.py
+++ CVprojectP2/videoprocessor.py
@@ -1,49 +1,129 @@
+# import cv2 as cv
+# import os
+# from matplotlib import pyplot as plt
+# from circlefinder import detect_and_draw_circles  # Assuming the function is in circlefinder module
+
+# filename = "coinTrack.avi"
+# capture = cv.VideoCapture(filename)
+# framecnt = 0
+# frame_count_list = []
+# center_xlist = []  
+# center_ylist = []
+# center_zlist = []  # List to store z-axis coordinates
+# plt.ion()  # Turn on interactive mode
+
+# if not os.path.exists('Frame_Dump'):
+#     os.mkdir('Frame_Dump')
+# else:
+#     print("Directory already exists, proceeding with overwriting directory\n")
+
+# try:
+#     while capture.isOpened():
+#         state, frame = capture.read()
+#         if state:
+#             framecnt += 1
+#             # Saving the frame as an image with a filename instead of a string
+#             frame_filename = f'Frame_Dump/frame_{framecnt:04d}.jpg'
+#             cv.imwrite(frame_filename, frame)
+
+#             # Call detect_and_draw_circles function to get center coordinates
+#             coordinates = detect_and_draw_circles(frame, framecnt)
+
+#             # If center coordinates are found, append them to the lists
+#             if coordinates is not None:
+#                 x, y, z = coordinates
+#                 frame_count_list.append(framecnt)
+#                 center_xlist.append(x)
+#                 center_ylist.append(y)
+#                 center_zlist.append(z)  # Append z-coordinate to the list
+#                 # Plot the center coordinates with respect to frame count
+#                 plt.clf()  # Clear the previous plot
+#                 plt.plot(frame_count_list, center_xlist, label='Center X Coordinate', marker='o', linestyle='-')
+#                 plt.plot(frame_count_list, center_ylist, label='Center Y Coordinate', marker='o', linestyle='-')
+#                 plt.plot(frame_count_list, center_zlist, label='Center Z Coordinate', marker='o', linestyle='-')
+#                 plt.xlabel('Frame Count')
+#                 plt.ylabel('Center Position')
+#                 plt.legend()
+#                 plt.title('Center Position over Frame Count')
+#                 plt.draw()
+#                 plt.pause(0.1)  # Adjust the pause duration as needed
+#             else:
+#                 print("No circles detected in frame ", framecnt)
+
+#             print('FrameCount:' + str(framecnt) + '\n')
+#         else:
+#             print("No remaining frames to process\n")
+#             break
+
+# except KeyboardInterrupt:
+#     print("Interrupted by user")
+
+# finally:
+#     plt.ioff()  # Turn off interactive mode
+#     capture.release()
+#     plt.show()
+
+
+
 import cv2 as cv
 import os
 from matplotlib import pyplot as plt
-from circlefinder import detect_and_draw_circles
+from circlefinder import detect_and_draw_circles  # Assuming the function is in circlefinder module
 
-filename = "videos/coin.mov"
+filename = "coin.mov"
 capture = cv.VideoCapture(filename)
 framecnt = 0
 frame_count_list = []
 center_xlist = []  
 center_ylist = []
+center_zlist = []  # List to store z-axis coordinates
 plt.ion()  # Turn on interactive mode
+# Set the desired size of the video window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
 if not os.path.exists('Frame_Dump'):
     os.mkdir('Frame_Dump')
 else:
     print("Directory already exists, proceeding with overwriting directory\n")
-
 try:
     while capture.isOpened():
         state, frame = capture.read()
         if state:
             framecnt += 1
-            # Saving the frame as an image with a filename instead of a string
-            frame_filename = f'Frame_Dump/frame_{framecnt:04d}.jpg'
-            cv.imwrite(frame_filename, frame)
+            # Resize the frame to the desired window size
+            frame = cv.resize(frame, (WINDOW_WIDTH, WINDOW_HEIGHT))
+            cv.imshow('Video', frame)
 
-            centerX, centerY = detect_and_draw_circles(frame, framecnt)
 
-            # If center x-coordinate is found, append it to the list
-            if centerX is not None and centerY is not None:
+            # Call detect_and_draw_circles function to get center coordinates
+            coordinates = detect_and_draw_circles(frame, framecnt)
+
+            # If center coordinates are found, append them to the lists
+            if coordinates is not None:
+                x, y, z = coordinates
                 frame_count_list.append(framecnt)
-                center_xlist.append(centerX)
-                center_ylist.append(centerY)
+                center_xlist.append(x)
+                center_ylist.append(y)
+                center_zlist.append(z)  # Append z-coordinate to the list
                 # Plot the center coordinates with respect to frame count
                 plt.clf()  # Clear the previous plot
                 plt.plot(frame_count_list, center_xlist, label='Center X Coordinate', marker='o', linestyle='-')
                 plt.plot(frame_count_list, center_ylist, label='Center Y Coordinate', marker='o', linestyle='-')
+                plt.plot(frame_count_list, center_zlist, label='Center Z Coordinate', marker='o', linestyle='-')
                 plt.xlabel('Frame Count')
                 plt.ylabel('Center Position')
                 plt.legend()
                 plt.title('Center Position over Frame Count')
                 plt.draw()
                 plt.pause(0.1)  # Adjust the pause duration as needed
+            else:
+                print("No circles detected in frame ", framecnt)
 
             print('FrameCount:' + str(framecnt) + '\n')
+            key = cv.waitKey(10)
+            if key & 0xFF == ord('q') or key == 27:  # Press 'q' or 'Esc' to exit
+                break
         else:
             print("No remaining frames to process\n")
             break
@@ -52,6 +132,5 @@ except KeyboardInterrupt:
     print("Interrupted by user")
 
 finally:
-    plt.ioff()  # Turn off interactive mode
     capture.release()
-    plt.show()
+    cv.destroyAllWindows()
